727f315cdf9af9c7e5a28af75d482586500b76c9
v0ncent <banksvincent5@gmail.com>
2024-04-16 12:41:32 -0500

    set up advantage kit logging.

---
+++ 2024RobotAdvantageKit/src/main/java/frc/robot/Subsystems/DriveTrain.java
@@ -0,0 +1,13 @@
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lib.DashboardConfiguration;
+
+public class DriveTrain extends SubsystemBase implements DashboardConfiguration {
+
+
+    @Override
+    public void configureDashboard() {
+
+    }
+}
